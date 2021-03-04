0522908a5bb6e83818ae2fe0168c85ef3ccd965a
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-04 09:18:34 +0530

    Added:flipcoin store in dict

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,8 +1,24 @@
-#!/bin/bash -x
-coin=$((RANDOM%2))
-if [ $coin -eq 1 ]
-then 
-     echo "Head"
-else 
-      echo "Tail"
-fi
+#!/bin/bash
+count=0
+H=0
+T=0
+while [ $count -lt 10 ]
+do 
+     coin=$((RANDOM%2))
+      if [ $coin -eq 1 ]
+      then
+	      	H=$(($H+1))
+            dict[$count]="HEADS"
+      else
+	       	T=$(($T+1))
+            dict[$count]="TAILS"
+       fi
+         ((count++))
+done
+echo ${dict[@]}
+echo "total heads are: " $H
+echo "total tails are: " $T
+percentageH=$((100*$H/10))
+percentageT=$((100*$T/10))
+echo "percentage of Heads is: " $percentageH
+echo "percentage of Tails is: " $percentageT
